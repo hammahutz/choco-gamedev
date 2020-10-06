44f5bb68a3e1a2ad5c916ddee36c283f61a54b62
hammahutz <mottz94@gmail.com>
2020-10-06 23:39:56 +0200

    Hello Testing

--- src/choco.ps1
+++ src/choco.ps1
@@ -1,4 +1,6 @@
-choco install bulk-crap-uninstaller ccleaner git visualstudio2019community visualstudio2019-workload-managedgame visualstudio2019-workload-manageddesktop visualstudio2019-workload-netcrossplat visualstudio2019-workload-nativemobile visualstudio2019-workload-universal unity-hub git unity unity-android unity-webgl unity-docs vscode krita gimp blender audacity godot-mono netfx-4.7-devpack dotnetcore-sdk inkscape -y
+choco install bulk-crap-uninstaller ccleaner git krita gimp blender audacity godot-mono godot netfx-4.7-devpack dotnetcore-sdk inkscape -y
 dotnet new -i Monogame.Templates.CSharp
+dotnet tool install -g dotnet-mgfxc
+dotnet tool install -g dotnet-mgcb
 dotnet tool install -g dotnet-mgcb-editor
 mgcb-editor --register
--- src/monogame.ps1
+++ src/monogame.ps1
@@ -1,4 +1,6 @@
 ﻿choco install dotnetcore-sdk -y
 dotnet new -i Monogame.Templates.CSharp
+dotnet tool install -g dotnet-mgfxc
+dotnet tool install -g dotnet-mgcb
 dotnet tool install -g dotnet-mgcb-editor
 mgcb-editor --register
